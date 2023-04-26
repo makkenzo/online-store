bdc69b54b90c80bafaf532042d846eef84d64e19
makkenzo <nekgo2009@gmail.com>
2023-04-26 16:13:08 +0600

    special offers

--- frontend/src/components/ProductBox.jsx
+++ frontend/src/components/ProductBox.jsx
@@ -1,44 +1,44 @@
-import chandelier from '../assets/special_offers/chandelier.png';
-import sofa from '../assets/special_offers/sofa.png';
-import dresser from '../assets/special_offers/dresser.png';
-
 import ProductCard from './ProductCard';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProductBox = () => {
-    const products = [
-        {
-            id: 1,
-            imageSrc: chandelier,
-            name: 'Люстра VODA',
-            desc: 'Основание из полированной нержавеющей стали, придает оригинальный парящий эффект.',
-            price: 45000,
-            oldPrice: 67000,
-        },
-        {
-            id: 2,
-            imageSrc: sofa,
-            name: 'Диван RONALD',
-            desc: 'Модель отличается простотой линий и форм, отсутствием броского декора.',
-            price: 156000,
-            oldPrice: 198000,
-        },
-        {
-            id: 3,
-            imageSrc: dresser,
-            name: 'Комод VENT',
-            desc: 'Европейский дуб - отличается особой прочностью и стабильностью.',
-            price: 34000,
-            oldPrice: 45000,
-        },
-    ];
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/api/products');
+                setData(response.data);
+                setLoading(false);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const sortedProducts = data.sort((a, b) => {
+        const diffA = a.oldPrice - a.price;
+        const diffB = b.oldPrice - b.price;
+        return diffB - diffA;
+    });
+
+    const displayedProducts = sortedProducts.slice(0, 3);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <section className="py-12 container mx-auto">
             <h2 className="text-4xl font-bold mb-8">Специальные предложения</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <ProductCard product={products[0]} />
-                <ProductCard product={products[1]} />
-                <ProductCard product={products[2]} />
+                {displayedProducts.map((product) => (
+                    <ProductCard key={product._id} product={product} />
+                ))}
             </div>
         </section>
     );
